e2a1d662d9c5f3322e9a4d7f652439a8729378ce
Ramiro Salcedo <ramirosdamian@gmail.com>
2021-06-19 04:24:25 -0300

    Update tp6ej3.py

--- tp6ej3.py
+++ tp6ej3.py
@@ -5,15 +5,15 @@
 ################
 
 def copiadora(ruta, ruta2):
-    
+    #abro el archivo de la ruta indicada#
     archivo = open(ruta, "r")
-    
+    #guardo el contenido en una variable#
     contenido = archivo.read()
-    
+    #leo el contenido de la variable y lo guardo en una nueva variable#
     nuevoarchivo = open(ruta2, "w")
-    
+    #creo el nuevo archivo en la ruta que se indico#
     nuevoarchivo.write(contenido)
-    
+    #escribo el archivo con el contenido que guarde previamente de el primer archivo que lei#
     return
 
 
